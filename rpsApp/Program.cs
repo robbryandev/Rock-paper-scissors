1d5a1dd1e02129716ce6836c9d49e3404851c25a
robbryandev <robbryandev@gmail.com>
2022-12-07 14:12:05 -0800

    Add cli

--- rpsApp/Program.cs
+++ rpsApp/Program.cs
@@ -4,8 +4,11 @@ namespace rpsApp
     {
         public static void Main()
         {
+            Console.WriteLine("Player 1 (rock, paper, scissors):");
             string p1 = Console.ReadLine();
+            Console.WriteLine("Player 2 (rock, paper, scissors):");
             string p2 = Console.ReadLine();
+            Console.WriteLine(rpsLogic.rpsWinCon(p1, p2));
         }
     }
 }
